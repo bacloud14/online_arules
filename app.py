110ffe58abbf1449a3a6cc048f9db74f9e35e121
bacloud14 <bacloud14@gmail.com>
2020-08-28 21:41:56 +0200

    ff post form inputs

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handleUpload():
       print(r.shape)
       r = list(r.itertuples(index=False, name=None))
       r = [tuple(filter(bool, tup)) for tup in r]
-      itemsets, rules = process(data = r[1:], min_support=request.files['min_support'],  min_confidence=request.files['min_confidence'])
+      itemsets, rules = process(data = r[1:], min_support=request.form['min_support'],  min_confidence=request.form['min_confidence'])
       rules = [rule for rule in rules if 'nan' not in str(rule)]
       return (jsonify(str(sorted(rules, key=lambda rule: rule.lift))))
   return redirect(url_for('fileFrontPage', #filename=filename
